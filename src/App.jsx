688b768af7eb595810d2230fdb4633eea400b829
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-13 22:37:57 +0100

    feat: create routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Notes from './pages/Notes';
+import LoginRegister from './pages/LoginRegister';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Notes />} path='/'></Route>
+          <Route element={<LoginRegister />} path='/login'></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
