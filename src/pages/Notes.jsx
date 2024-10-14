acf5a8f92924b74f97831e80959811735b9d3cd0
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-14 07:36:52 +0100

    feat: add initialize function to fetch from appwrite db

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { databases } from '../appwrite/config';
 
 function Notes() {
+  // Initialize component functions
+  const init = async () => {
+    const response = await databases.listDocuments(
+      import.meta.env.VITE_COLLECTION_ID
+    );
+    console.log(response);
+  };
+
   return <div>Notes</div>;
 }
 
