867ac69c34b0d65b93d1a792d0db8443d243dba7
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-14 07:07:59 +0100

    feat: create appwrite config

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,1 +1,11 @@
-import { Client } from 'appwrite';
+import { Client, Databases } from 'appwrite';
+
+const client = new Client();
+
+client
+    .setEndpoint(import.meta.env.VITE_ENDPOINT);
+    .setProject(import.meta.env.VITE_PROJECT_ID);
+
+const database = new Databases(client);
+
+export {client, databases};
