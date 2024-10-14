88ced04cd29845bafd57777acc7c8674f1ef3d63
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-14 07:57:51 +0100

    feat: implement simple get function from Appwrite DB

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -3,9 +3,9 @@ import { Client, Databases } from 'appwrite';
 const client = new Client();
 
 client
-    .setEndpoint(import.meta.env.VITE_ENDPOINT);
-    .setProject(import.meta.env.VITE_PROJECT_ID);
+  .setEndpoint(import.meta.env.VITE_ENDPOINT)
+  .setProject(import.meta.env.VITE_PROJECT_ID);
 
 const database = new Databases(client);
 
-export {client, databases};
+export { client, database };
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,16 +1,34 @@
 import { useEffect, useState } from 'react';
-import { databases } from '../appwrite/config';
+import { database } from '../appwrite/config';
 
 function Notes() {
+  const [notes, setNotes] = useState([]);
+
+  // Run the init function when the component mounts
+  useEffect(() => {
+    init();
+  }, []);
+
   // Initialize component functions
   const init = async () => {
-    const response = await databases.listDocuments(
-      import.meta.env.VITE_COLLECTION_ID
+    const response = await database.listDocuments(
+      import.meta.env.VITE_DATABASE_ID,
+      import.meta.env.VITE_COLLECTION_ID_NOTES
     );
-    console.log(response);
+
+    setNotes(response.documents);
   };
 
-  return <div>Notes</div>;
+  return (
+    <div>
+      <div>
+        <h1>Notes</h1>
+        {notes.map((note) => (
+          <div key={note.$id}>{note.body}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Notes;
